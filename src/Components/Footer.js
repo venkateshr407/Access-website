d1c9c5a85a5149ba42cff15f6156270ddcb0b7ca
venkateshr407 <venkateshr590@gmail.com>
2020-02-17 21:39:15 +0530

    second  commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,11 +6,11 @@ export default class Footer extends Component {
             <div className="footer">
                 <div className="grid-container">
                     <div className="footer-about">
-                        <h1 data-aos="fade-up">About us</h1>
-                        <p data-aos="fade-up">We are a bunch of professionl's from various discipline's
+                        <h1>About us</h1>
+                        <p>We are a bunch of professionl's from various discipline's
                          who are trying to bring change in the tech education.</p>
                     </div>
-                    <div className="footer-forms" data-aos="fade-up">
+                    <div className="footer-forms">
 
                         <h1>Technologies</h1>
                         <ul>
@@ -20,15 +20,15 @@ export default class Footer extends Component {
                             <li><a href="https://reactjs.org/">React.js</a></li>
                          </ul>
                     </div>
-                    <div className="footer-items" data-aos="fade-up">
-                        <h1 data-aos="fade-up">Get in touch </h1>
+                    <div className="footer-items">
+                        <h1>Get in touch </h1>
                         <p class="footpara">Mail:<a href="mailto:access@codeonetech.com"> access@codeonetech.com </a></p>
                         <p class="footpara">Phone: +91 8147564935 , +91 9535386618</p>
                     </div>
                     
                     <div className="footer-about">
-                        <h1 data-aos="fade-up">Developed by:</h1>
-                        <p data-aos="fade-up"><span class="access"><a  href="https://venkycreations.netlify.com/" className="footer-dev">Venkatesh.R</a></span>
+                        <h1>Developed by:</h1>
+                        <p><span class="access"><a  href="https://venkycreations.netlify.com/" className="footer-dev">Venkatesh.R</a></span>
                         <br/>Access-101 Student</p>
                      <span className="move-up"><a class="btn" href="#"><i class="fa fa-arrow-circle-up fa-2x " aria-hidden="true"></i></a></span>
                     </div>
